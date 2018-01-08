c5e64b24adbf3cae8013553158bf4494354342fc
McKenna Jo Beardsley <mckennajo@McKennas-MacBook-Pro.local>
2018-01-07 23:53:37 -0800

    second update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,13 +4,28 @@ $(document).ready(function(){
   var wrong = 0;
   var timer = 30;
 
+console.log(correct);
+console.log(wrong);
+console.log(timer);
+
   // start GAME
 
+
+
   // end GAME
+
+
   // game ends if:
+
+
   // time runs out or user submits
+
 
   // start a timer
 
+
   // display number correct and number wrong
 });
+window.onload = function() {
+
+};
